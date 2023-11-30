83a2f26fa9b0c6db09e268d4f9ab359a7c46b2d0
mdmonir-hossain <monirsoftengr@gmail.com>
2023-11-30 19:26:40 +0600

    Success Counter get

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xzwqfr8.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,8 +21,38 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-   
+    const biodatasCollection = client.db("biodatasdb").collection("biodatas");
+
+    app.get("/biodatas", async (req, res) => {
+      const cursor = biodatasCollection
+        .find({ AccountType: "Premium" })
+        .limit(6);
+        ;
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/maleCounter", async (req, res) => {
+      const cursor = biodatasCollection.find({ BiodataType: "Male" });
+        ;
+        ;
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/femaleCounter", async (req, res) => {
+      const cursor = biodatasCollection
+        .find({ BiodataType: "Female" })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/marriagesCompleted", async (req, res) => {
+      const cursor = biodatasCollection
+        .find({ MarriageCompleted: "Yes" })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,13 +60,9 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    
   }
 }
 run().catch(console.dir);
-
-
-
 
 app.get("/", (req, res) => {
   res.send("server is running");
